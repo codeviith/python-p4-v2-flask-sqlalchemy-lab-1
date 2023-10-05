15e66b8a82ac2cdd7d0dad901fe163ecb46ee8ef
codeviith <codeviith@Cinya.>
2023-10-05 10:24:28 -0400

    phase-4-flask-sqlalchemy-lab-1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,23 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def show_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        return make_response(earthquake.to_dict(), 200)
+    else:
+        return make_response({"message": f"Earthquake {id} not found."}, 404) 
+                        ## {} in front of "messages" is to turn it into a dict.
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_quakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquakes_list = [quake.to_dict() for quake in earthquakes]
+
+    return make_response({"count": len(earthquakes), "quakes": earthquakes_list}, 200)
+
 
 
 if __name__ == '__main__':
